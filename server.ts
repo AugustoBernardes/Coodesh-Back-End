6808e34b08e6d4ba102573e3efbf8fe68c9a2379
AugustoBernardes <augustobdev@outlook.com>
2022-04-19 14:48:51 -0300

    feature - Adding global error treatement

--- server.ts
+++ server.ts
@@ -1,1 +1,39 @@
-console.log('dsad')
+import { Request,Response, NextFunction } from "express";
+import body_parser from "body-parser";
+import express from "express"
+import "express-async-errors"
+
+// Dotenv variables
+require('dotenv').config()
+const PORT = process.env.PORT
+
+const app = express();
+
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: false }));
+
+
+// Treating global error
+app.use((error:Error, req:Request, response:Response, next:NextFunction) => {
+    if(error instanceof Error){
+        return (
+            response.status(400),
+            response.json({
+                status:400,
+                error:error.message
+            })
+        )
+    }
+    
+    return (
+        response.status(500),
+        response.json({
+            status:500,
+            error: "Internal Server Error",
+        })
+    )
+
+})
+
+// Running server
+app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}!`))
